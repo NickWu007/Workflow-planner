603c86cfca4e237a03bde37829d7a72f50f04470
Kathryn Rendell <kathrynrendell@hotmail.com>
2016-12-03 10:54:56 -0500

    Updated login/signup page

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+    var images = ['img/login1.JPG', 'img/login2.JPG', 'img/login3.JPG'];
+    var randomNumber = Math.floor(Math.random() * images.length);
+    var bgImg = 'url(' + images[randomNumber] + ')';
+    $('body').css({'background-image':bgImg})
+
     $('#sign-up-form').bootstrapValidator({
         // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
         feedbackIcons: {
