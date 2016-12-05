31bcb0b9ae543f222ad38f144f29a20d277e016f
gmcdonagh <gregmcd@live.unc.edu>
2016-12-04 19:45:39 -0500

    Update login.php

--- api/login.php
+++ api/login.php
@@ -1,41 +1,56 @@
 <?php
-require_once('model.php');
+session_start();
+
 header("Access-Control-Allow-Origin: *");
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    
+    function check_password($username, $password) {
+        $fd = fopen('hidden/passwords.txt', 'r');
+        while ($next_line = fgets($fd)) {
+            list($uname, $usalt, $uhash) = explode(' ', trim($next_line));
+            if ($uname == $username) {
+                if (md5($usalt . $password) == $uhash) {
+	               fclose($fd);
+	               return true;
+                }
+            }
+        }
+        
+    fclose($fd);
+    return false;
+    }
+     
     $post = json_decode(file_get_contents("php://input"), true);
     $username = $post['username'];
     $password = $post['password'];
 
-    if (!is_null($password) && !is_null($username)) {
-
-        $user = User::findByName($username);
-
-        if (is_null($user)) {
-            header("HTTP/1.1 404 Bad Request");
-            print("User doesn't exist");
-            exit();
-        }
-
-        if ($user->getPassword() != $password) {
-            header("HTTP/1.1 401 Unauthorized");
-            print("Username or password incorrect.");
-            exit();
-        }
-
-        $output = array(
-          'userID' => $user->getID()
-        );
-        
-        setcookie("user", $user->getID());
+    if (check_password($username, $password)) {
         header("Content-type: application/json");
-        header("HTTP/1.1 200 OK");
-        print(json_encode($output));
+
+        $_SESSION['username'] = $username;
+        $_SESSION['authsalt'] = time();
+        $auth_cookie_val = md5($_SESSION['username'] . $_SERVER['REMOTE_ADDR'] . $_SESSION['authsalt']);
+        setcookie('workflow_auth', $auth_cookie_val, 0, '/Courses/comp426-f16/users/gregmcd', 'wwwp.cs.unc.edu', true);
+
+        /* Add code here to return user id of person that logged in */
+        
         exit();
+    }
+
+    else {
+      unset($_SESSION['username']);
+      unset($_SESSION['authsalt']);
+
+      header('HTTP/1.1 401 Unauthorized');
+      header('Content-type: application/json');
+      print(json_encode(false));
+      exit();
     }
 }
 
 header("HTTP/1.1 400 Bad Request");
 print("Format not recognized");
 exit();
+
 ?>
