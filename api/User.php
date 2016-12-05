90159c5e48be12242ac6cb0979370b5063d730ce
gmcdonagh <gregmcd@live.unc.edu>
2016-12-04 21:12:50 -0500

    Update User.php

--- api/User.php
+++ api/User.php
@@ -1,4 +1,3 @@
-<?php
 class User {
   private $id;
   private $name;
@@ -11,7 +10,7 @@ class User {
   public static function create($name, $email, $password) {
     $mysqli = User::connect();
     $result = $mysqli->query("insert into User values (0, \"" . 
-                           $name . "\", \"" . $email . "\", \"" . $password . "\")");
+                           $name . "\", \"" . $password . "\", \"" . $email . "\")");
     if ($result) {
       $new_id = $mysqli->insert_id;
       return new User($new_id, $name, $email, $password);
@@ -22,7 +21,7 @@ class User {
   public static function findByID($id) {
     $mysqli = User::connect();
     
-    $result = $mysqli->query("select * from User where id = " . $id );
+    $result = $mysqli->query("select * from User where id = \"" . $id . "\"");
     if ($result) {
       if ($result->num_rows == 0){
         return null;
@@ -99,7 +98,7 @@ class User {
   
   public function delete() {
     $mysqli = User::connect();
-    $result = $mysqli->query("delete FROM User where id = " . $this->id);
+    $result = $mysqli->query("delete FROM User where id = \"$this->id\"");
     return $result;
   }
 }
