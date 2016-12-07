2632de46d8b0d864f5cf038b19532be6bbf4cc2b
gmcdonagh <gregmcd@live.unc.edu>
2016-12-06 20:32:26 -0500

    Update welcome.php

--- api/welcome.php
+++ api/welcome.php
@@ -1,10 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN'] . "");
+header("Access-Control-Allow-Credentials : true");
 session_start();
-
 require_once('authenticate.php');
 require_once('User.php');
 
-header("Access-Control-Allow-Origin: *");
 header("HTTP/1.1 200 Successfuly logged in");
 $username = $_SESSION['username'];
 $user = User::findByName($username);
